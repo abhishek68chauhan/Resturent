2018c1ddce43c33177b0361f8c4ef27ec55c005d
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-16 12:50:00 +0530

    feat: Create About component for company information section

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const About = () => {
+    return (
+        <section className="about" id="about">
+            <div className="container">
+                <div className="banner">
+                    <div className="top">
+                        <h1 className="heading">About Us</h1>
+                        <p>The only thing we are serious about is food.</p>
+                    </div>
+                    <p className="mid">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni in sint, cumque necessitatibus harum quis nihil, qui pariatur laudantium incidunt odit laboriosam repellendus! Corrupti velit, commodi iste tenetur accusamus eum.
+                    </p>
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
