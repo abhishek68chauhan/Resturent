9b5b13f921572b43ad669767e4a76dc39f4f73c2
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-17 19:36:08 +0530

    style: Update heading text to uppercase for consistency in Menu component

--- Frontend/src/components/Menu.jsx
+++ Frontend/src/components/Menu.jsx
@@ -5,7 +5,7 @@ export const Menu = () => {
         <section className='menu' id='menu'>
             <div className="container">
                 <div className="heading_section">
-                    <h1 className='heading'>Popular Dishes</h1>
+                    <h1 className='heading'>POPULAR DISHES</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quos tempore, expedita reiciendis sunt dolore eveniet architecto rerum dolorum accusantium odit adipisci quo quidem, facilis quaerat similique modi hic debitis. </p>
                 </div>
                 <div className="dishes_container">
