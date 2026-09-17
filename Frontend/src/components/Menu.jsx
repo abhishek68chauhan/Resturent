63541a80bd1aac1bfb041adf0986bf4b8c6e0384
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-17 19:26:08 +0530

    feat: Implement Menu component to display popular dishes section

--- Frontend/src/components/Menu.jsx
+++ Frontend/src/components/Menu.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import {data} from '../restApi.json'
 export const Menu = () => {
-    return (
-        <div>Menu</div>
-    )
+    return(
+        <section className='menu' id='menu'>
+            <div className="container">
+            <div className="heading_section">
+                <h1 className='heading'>Popular Dishes</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quos tempore, expedita reiciendis sunt dolore eveniet architecto rerum dolorum accusantium odit adipisci quo quidem, facilis quaerat similique modi hic debitis. </p>
+            </div>
+            </div>
+        </section>   
+    )   
 }
