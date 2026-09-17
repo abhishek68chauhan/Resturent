a0abbe7484f2a679cdfcd667ffb0f16caabba531
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-17 19:30:35 +0530

    style: Refactor Menu component for improved code readability and structure

--- Frontend/src/components/Menu.jsx
+++ Frontend/src/components/Menu.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import {data} from '../restApi.json'
+import { data } from '../restApi.json'
 export const Menu = () => {
-    return(
+    return (
         <section className='menu' id='menu'>
             <div className="container">
-            <div className="heading_section">
-                <h1 className='heading'>Popular Dishes</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quos tempore, expedita reiciendis sunt dolore eveniet architecto rerum dolorum accusantium odit adipisci quo quidem, facilis quaerat similique modi hic debitis. </p>
+                <div className="heading_section">
+                    <h1 className='heading'>Popular Dishes</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quos tempore, expedita reiciendis sunt dolore eveniet architecto rerum dolorum accusantium odit adipisci quo quidem, facilis quaerat similique modi hic debitis. </p>
+                </div>
+                <div className="dishes_container">
+                    {
+                        data[0].dishes.map(element => {
+                            return (
+                                <div className="card" key={element.id}>
+                                    <img src={element.image} alt={element.title} />
+                                    <h3>{element.title}</h3>
+                                    <button>{element.category}</button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
-            </div>
-        </section>   
-    )   
+        </section>
+    )
 }
