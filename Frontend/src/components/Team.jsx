4cede3e8f584b9de9e3a73b025db03684aa3b2db
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 19:10:02 +0530

    fix: Ensure team member names are displayed in the Team component cards

--- Frontend/src/components/Team.jsx
+++ Frontend/src/components/Team.jsx
@@ -13,6 +13,7 @@ const Team = () => {
                         return(
                             <div className="card" key={element.id}>
                                 <img src={element.image} alt={element.name} />
+                                <h3>{element.name}</h3>
                             </div>
                         )
                     })
