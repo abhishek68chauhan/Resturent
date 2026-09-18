6ea16b06e600276cec2748c105e2a53de68324cf
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 18:59:04 +0530

    fix: Format code for consistency in Team component

--- Frontend/src/components/Team.jsx
+++ Frontend/src/components/Team.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {data} from "../restApi.json";
+import { data } from "../restApi.json";
 const Team = () => {
-  return (
-    <div>Team</div>
-  )
+    return (
+        <div>Team</div>
+    )
 }
 
 export default Team;
