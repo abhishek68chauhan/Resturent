43e5f5aafb4c9bef24b7d48021fa29ca0db8a6ac
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 12:54:36 +0530

    fix: Correct import statement formatting for WhoAreWe component in Home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import About from '../components/About';
 import HeroSection from '../components/HeroSection';
 import { Menu } from '../components/Menu';
 import Qualities from '../components/Qualities';
-import WhoAreWe  from "../components/WhoAreWe";
+import WhoAreWe from "../components/WhoAreWe";
 
 const Home = () => {
   return (
