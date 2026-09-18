8b138f2bc753c20648884b9fec803b75fbff264b
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 21:37:46 +0530

    fix: Change initial state of phone to a number in Reservation component

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -11,7 +11,7 @@ const Reservation = () => {
     const [email, setEmail] = useState(" ");
     const [date, setDate] = useState(" ");
     const [time, setTime] = useState(" ");
-    const [phone, setPhone] = useState(" ");
+    const [phone, setPhone] = useState(0);
 
     return <section className='reservation' id='reservation'></section>
 };
