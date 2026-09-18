58f477fffc6158449560275a0bcde0622293fead
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 19:29:10 +0530

    fix: Add missing useState import in Reservation component

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';     
 import axios from "react";
+import {useState} from 'react';
 
 const Reservation = () => {
   return (
