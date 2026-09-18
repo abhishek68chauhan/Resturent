9335ecb3934168f7f4dc61461975791f16eb2384
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 21:26:13 +0530

    fix: Format import statements and indentation in Reservation component for consistency

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HiOutlineArrowNarrowRight } from 'react-icons/hi';     
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 import axios from "react";
-import {useState} from 'react';
+import { useState } from 'react';
 
 const Reservation = () => {
-  return (
-    <section className='reservation' id='reservation'>
+    return (
+        <section className='reservation' id='reservation'>
 
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Reservation;
