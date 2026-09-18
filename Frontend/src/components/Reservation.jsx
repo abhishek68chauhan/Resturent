51d5c980c488e54c4b4e8bcfeef35d11d6e2a3b5
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 21:34:47 +0530

    fix: Add useState hook for firstName in Reservation component

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -6,6 +6,8 @@ import toast from "react-hot-toast";
 import { useNavigat } from "react-router-dom";
 
 const Reservation = () => {
+    const [firstName, setFirstName] = useState(" ");
+
     return <section className='reservation' id='reservation'></section>
 };
 
