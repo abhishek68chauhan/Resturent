581c1c7ca8a9c45eb5c7cb18b6e1d487c603be18
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 19:20:53 +0530

    fix: Add class name to heading in Team component for styling consistency

--- Frontend/src/components/Team.jsx
+++ Frontend/src/components/Team.jsx
@@ -4,7 +4,7 @@ const Team = () => {
     return <section className='team' id='team'>
         <div className="container">
             <div className="heading_section">
-                <h1>OUR TEAM</h1>
+                <h1 className='heading'>OUR TEAM</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis delectus illo fugiat ipsum. Explicabo totam quam earum doloribus repudiandae non, magni suscipit laboriosam ipsam, sunt nemo beatae eveniet nesciunt unde.</p>
             </div>
             <div className="team_container">
