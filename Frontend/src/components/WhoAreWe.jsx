3bd9846a8be2acfab1218ccbd66adfd6924b6852
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 13:07:36 +0530

    fix: Add additional text banner to WhoAreWe component for displaying more entries

--- Frontend/src/components/WhoAreWe.jsx
+++ Frontend/src/components/WhoAreWe.jsx
@@ -20,6 +20,18 @@ const WhoAreWe = () => {
                     <img src="/center.svg" alt="center" className='gradient_bg' />
                     <img src="/whoweare.png" alt="who" />
                 </div>
+                <div className="text_banner">
+                    {
+                        data[0].who_we_are.slice(2).map(element => {
+                            return (
+                                <div className="card" key={element.id}>
+                                    <h1 style={{ fontWeight: "300" }} className='heading'>{element.number}</h1>
+                                    <p>{element.id}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </section>
     )
