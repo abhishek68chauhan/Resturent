d72c2f15a631592b94d5af80a972bfc524b1f14c
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 19:11:22 +0530

    feat: Add Team component to Home page for team member display

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import About from '../components/About';
 import HeroSection from '../components/HeroSection';
 import { Menu } from '../components/Menu';
 import Qualities from '../components/Qualities';
+import Team from '../components/Team';
 import WhoAreWe from "../components/WhoAreWe";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <Qualities />
       <Menu />
       <WhoAreWe />
+      <Team />
     </>
   )
 }
