aed777f4711372e2de2ac934d1d41273c6a778b0
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-18 21:31:30 +0530

    fix: Simplify return statement in Reservation component

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -6,11 +6,7 @@ import toast from "react-hot-toast";
 import { useNavigat } from "react-router-dom";
 
 const Reservation = () => {
-    return (
-        <section className='reservation' id='reservation'>
-
-        </section>
-    )
-}
+    return <section className='reservation' id='reservation'></section>
+};
 
 export default Reservation;
