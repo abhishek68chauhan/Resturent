48fdf2a9d3839778b0dcc554b7ba76fe8f2891c5
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-20 11:29:13 +0530

    fix: Update Footer component to display correct restaurant name

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
         <div className="container">
             <div className="banner">
-                <div className="left">Abhishek Chauhan</div>
+                <div className="left">RESTURENT</div>
                 <div className="right">
                     <p>VBSPU, Jaunpur</p>
                     <p>Open: 10:00 AM - 05:00 PM</p>
