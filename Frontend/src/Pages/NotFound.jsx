8b776f2e923f90f4af729a65bd06f19786915a99
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-20 11:37:20 +0530

    fix: Update NotFound component to include missing heading and structure

--- Frontend/src/Pages/NotFound.jsx
+++ Frontend/src/Pages/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';     
 
 const NotFound = () => {
   return (
     <>
-      <h1>NOT FOUND PAGE</h1>
+      <section className='notFound'>
+        <div className="container">
+          <img src="/notFound.svg" alt="notFound" />
+          
+        </div>
+      </section>
     </>
   )
 }
