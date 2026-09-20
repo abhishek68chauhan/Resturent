2d183ed6480a3c7b8d8718fdd735ef0d79572fd5
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-20 11:41:01 +0530

    fix: Add arrow icon to back link in NotFound component

--- Frontend/src/Pages/NotFound.jsx
+++ Frontend/src/Pages/NotFound.jsx
@@ -10,7 +10,7 @@ const NotFound = () => {
           <img src="/notFound.svg" alt="notFound" />
           <h1>LOOKS LIKE YOU'RE LOST</h1>
           <p>We can not seen to find you the page you're looking for</p>
-          <Link to={'/'}>Back to Home </Link>
+          <Link to={'/'}>Back to Home <span><HiOutlineArrowNarrowRight /></span></Link>
         </div>
       </section>
     </>
