aa80b0c7d7bb52fabd0c2c0847288a7ee2702a3d
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-20 11:23:07 +0530

    fix: Refactor Footer component structure for improved layout

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,7 +4,15 @@ const Footer = () => {
   return (
     
     <footer>
-        
+        <div className="container">
+            <div className="banner">
+                <div className="left">Abhishek Chauhan</div>
+                <div className="right">
+                    <p>VBSPU, Jaunpur</p>
+                    <p>Open: 10:00 AM - 05:00 PM</p>
+                </div>
+            </div>
+        </div>
 
 
 
