0ebf82f0e0fc5fec2f34faf2d7f720ef3ab846fb
abhishek68chauhan <abhishekchauhan.06082004@gmail.com>
2026-09-20 09:04:58 +0530

    fix: Update server configuration to use default port fallback

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,17 @@
+// import app from "./app.js";
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
+
+
+import dotenv from "dotenv";
 import app from "./app.js";
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
